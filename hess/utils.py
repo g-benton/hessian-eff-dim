28498a8cd0766198ed6af9f1a80560b1e10756d9
g-benton <greg.w.benton@gmail.com>
2019-11-04 17:29:29 -0500

    utils first try

---
+++ hess/utils.py
@@ -0,0 +1,73 @@
+import torch
+import time
+import numpy as np
+from torch import nn
+from torch.autograd import Variable
+
+from gpytorch.utils.lanczos import lanczos_tridiag, lanczos_tridiag_to_diag
+
+def unflatten_like(vector, likeTensorList):
+    # Takes a flat torch.tensor and unflattens it to a list of torch.tensors
+    #    shaped like likeTensorList
+    outList = []
+    i = 0
+    for tensor in likeTensorList:
+        # n = module._parameters[name].numel()
+        n = tensor.numel()
+        outList.append(vector[:, i : i + n].view(tensor.shape))
+        i += n
+    return outList
+
+################################################################################
+#                              Supporting Functions
+################################################################################
+def gradtensor_to_tensor(net, include_bn=False):
+    """
+        convert the grad tensors to a list
+    """
+    filter = lambda p: include_bn or len(p.data.size()) > 1
+    return flatten([p.grad.data for p in net.parameters() if filter(p)])
+
+
+################################################################################
+#                  For computing Hessian-vector products
+################################################################################
+def eval_hess_vec_prod(vec, params, net, criterion, dataloader, use_cuda=False):
+    """
+    Evaluate product of the Hessian of the loss function with a direction vector "vec".
+    The product result is saved in the grad of net.
+    Args:
+        vec: a list of tensor with the same dimensions as "params".
+        params: the parameter list of the net (ignoring biases and BN parameters).
+        net: model with trained parameters.
+        criterion: loss function.
+        dataloader: dataloader for the dataset.
+        use_cuda: use GPU.
+    """
+
+    if use_cuda:
+        net.cuda()
+        vec = [v.cuda() for v in vec]
+
+    net.eval()
+    net.zero_grad()  # clears grad for every parameter in the net
+
+    for batch_idx, (inputs, targets) in enumerate(dataloader):
+        inputs, targets = Variable(inputs), Variable(targets)
+        if use_cuda:
+            inputs, targets = inputs.cuda(), targets.cuda()
+
+        outputs = net(inputs)
+        loss = criterion(outputs, targets)
+        grad_f = torch.autograd.grad(loss, inputs=params, create_graph=True)
+
+        # Compute inner product of gradient with the direction vector
+        # prod = Variable(torch.zeros(1)).type(type(grad_f[0].data))
+        prod = torch.zeros(1, dtype=grad_f[0].dtype, device=grad_f[0].device)
+        for (g, v) in zip(grad_f, vec):
+            prod = prod + (g * v).sum()
+
+        # Compute the Hessian-vector product, H*v
+        # prod.backward() computes dprod/dparams for every parameter in params and
+        # accumulate the gradients into the params.grad attributes
+        prod.backward()
