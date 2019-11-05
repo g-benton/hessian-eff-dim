dbbab746433cdfd722046ae1ddcebaf5be79f683
Greg Benton <greg_b@mint.orie.cornell.edu>
2019-11-05 09:48:53 -0800

    updating nbs

--- hess/nets/simple_net.py
+++ hess/nets/simple_net.py
@@ -4,12 +4,15 @@ from torch import nn
 
 class SimpleNet(nn.Module):
     """docstring for SimpleNet."""
-    def __init__(self, G_dat, D_dat, hidden_size=10, n_hidden=2):
+    def __init__(self, G_dat, D_dat, hidden_size=10, n_hidden=2,
+                activation=torch.nn.ReLU()):
         super(SimpleNet, self).__init__()
         self.G_dat = G_dat #inputs in G space
         self.D_dat = D_dat #inputs in D space
-
-        self.input_size = G_dat.size(1) # G = [n x dim x time]
+        if self.G_dat.ndim == 1:
+            self.input_size=1
+        else:
+            self.input_size = G_dat.size(1) # G = [n x dim x time]
         if self.D_dat.ndim == 1:
             self.output_size = 1 # D = [n x dim x time]
         else:
@@ -19,10 +22,10 @@ class SimpleNet(nn.Module):
         module = nn.ModuleList()
         module.append(nn.Linear(self.input_size, hidden_size))
         for ll in range(n_hidden):
-            module.append(nn.ReLU())
+            module.append(activation)
             module.append(nn.Linear(hidden_size, hidden_size))
 
-        module.append(nn.ReLU())
+        module.append(activation)
         module.append(nn.Linear(hidden_size, self.output_size))
 
         self.sequential = nn.Sequential(*module)
