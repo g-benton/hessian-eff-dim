3822cbb634abe0c4a494a13b55970265c0a6dc28
g-benton <greg.w.benton@gmail.com>
2019-11-11 12:49:49 -0500

    adding lstm

--- hess/nets/__init__.py
+++ hess/nets/__init__.py
@@ -1,3 +1,4 @@
 from .simple_net import *
 from .transformer import *
 from .cifar_net import *
+from .simple_lstm import *
---
+++ hess/nets/simple_lstm.py
@@ -0,0 +1,37 @@
+import torch
+import math
+from torch import nn
+
+class LSTM(nn.Module):
+
+    def __init__(self, x, y, n_hidden=2,  hidden_size=10,
+                 input_dim=1, output_dim=1,
+                  batch_size=1):
+        super(LSTM, self).__init__()
+        self.input_dim = x.size(-1)
+        self.hidden_dim = hidden_size
+        self.batch_size = batch_size
+        self.num_layers = n_hidden
+
+        # Define the LSTM layer
+        self.lstm = nn.LSTM(self.input_dim, self.hidden_dim, self.num_layers)
+
+        # Define the output layer
+        self.linear = nn.Linear(self.hidden_dim, y.size(-1))
+
+    def init_hidden(self):
+        # This is what we'll initialise our hidden state as
+        return (torch.zeros(self.num_layers, self.batch_size, self.hidden_dim),
+                torch.zeros(self.num_layers, self.batch_size, self.hidden_dim))
+
+    def forward(self, input):
+        # Forward pass through LSTM layer
+        # shape of lstm_out: [input_size, batch_size, hidden_dim]
+        # shape of self.hidden: (a, b), where a and b both
+        # have shape (num_layers, batch_size, hidden_dim).
+        lstm_out, self.hidden = self.lstm(input.view(len(input), self.batch_size, -1))
+
+        # Only take the output from the final timetep
+        # Can pass on the entirety of lstm_out to the next layer if it is a seq2seq prediction
+        y_pred = self.linear(lstm_out[-1].view(self.batch_size, -1))
+        return y_pred.view(-1)
