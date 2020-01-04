aafe34ea05ffe8d40eaa86ad30b3c57b4e738c87
Wesley Maddox <wm326@cornell.edu>
2020-01-04 13:44:14 -0500

    starting to work on the script

---
+++ experiments/parser.py
@@ -0,0 +1,147 @@
+import argparse
+
+def parser():
+    parser = argparse.ArgumentParser(description="Random Training")
+    parser.add_argument(
+        "--dir",
+        type=str,
+        default=None,
+        required=True,
+        help="training directory (default: None)",
+    )
+
+    parser.add_argument(
+        "--dataset", type=str, default="CIFAR10", help="dataset name (default: CIFAR10)"
+    )
+    parser.add_argument(
+        "--data_path",
+        type=str,
+        default=None,
+        required=True,
+        metavar="PATH",
+        help="path to datasets location (default: None)",
+    )
+    parser.add_argument(
+        "--use_test",
+        dest="use_test",
+        action="store_true",
+        help="use test dataset instead of validation (default: False)",
+    )
+    parser.add_argument("--split_classes", type=int, default=None)
+    parser.add_argument(
+        "--batch_size",
+        type=int,
+        default=128,
+        metavar="N",
+        help="input batch size (default: 128)",
+    )
+    parser.add_argument(
+        "--num_workers",
+        type=int,
+        default=4,
+        metavar="N",
+        help="number of workers (default: 4)",
+    )
+    parser.add_argument(
+        "--model",
+        type=str,
+        default=None,
+        required=True,
+        metavar="MODEL",
+        help="model name (default: None)",
+    )
+
+    parser.add_argument(
+        "--resume",
+        type=str,
+        default=None,
+        metavar="CKPT",
+        help="checkpoint to resume training from (default: None)",
+    )
+
+    parser.add_argument(
+        "--epochs",
+        type=int,
+        default=200,
+        metavar="N",
+        help="number of epochs to train (default: 200)",
+    )
+    parser.add_argument(
+        "--save_freq",
+        type=int,
+        default=25,
+        metavar="N",
+        help="save frequency (default: 25)",
+    )
+    parser.add_argument(
+        "--eval_freq",
+        type=int,
+        default=5,
+        metavar="N",
+        help="evaluation frequency (default: 5)",
+    )
+    parser.add_argument(
+        "--lr_init",
+        type=float,
+        default=0.01,
+        metavar="LR",
+        help="initial learning rate (default: 0.01)",
+    )
+    parser.add_argument(
+        "--momentum",
+        type=float,
+        default=0.9,
+        metavar="M",
+        help="SGD momentum (default: 0.9)",
+    )
+    parser.add_argument(
+        "--wd", type=float, default=1e-4, help="weight decay (default: 1e-4)"
+    )
+
+    parser.add_argument("--swa", action="store_true", help="swa usage flag (default: off)")
+    parser.add_argument(
+        "--swa_start",
+        type=float,
+        default=161,
+        metavar="N",
+        help="SWA start epoch number (default: 161)",
+    )
+    parser.add_argument(
+        "--swa_lr", type=float, default=0.02, metavar="LR", help="SWA LR (default: 0.02)"
+    )
+    parser.add_argument(
+        "--swa_c_epochs",
+        type=int,
+        default=1,
+        metavar="N",
+        help="SWA model collection frequency/cycle length in epochs (default: 1)",
+    )
+    parser.add_argument("--cov_mat", action="store_true", help="save sample covariance")
+    parser.add_argument(
+        "--max_num_models",
+        type=int,
+        default=20,
+        help="maximum number of SWAG models to save",
+    )
+
+    parser.add_argument(
+        "--swa_resume",
+        type=str,
+        default=None,
+        metavar="CKPT",
+        help="checkpoint to restor SWA from (default: None)",
+    )
+    parser.add_argument(
+        "--loss",
+        type=str,
+        default="CE",
+        help="loss to use for training model (default: Cross-entropy)",
+    )
+
+    parser.add_argument(
+        "--seed", type=int, default=1, metavar="S", help="random seed (default: 1)"
+    )
+    parser.add_argument("--no_schedule", action="store_true", help="store schedule")
+
+    args = parser.parse_args()
+    return args
--- experiments/test_subnets_cifar.py
+++ experiments/test_subnets_cifar.py
@@ -5,27 +5,49 @@ import hess.utils as utils
 import hess.nets
 import numpy as np
 import pickle
+import argparse
+import os, sys
 
-def twospirals(n_points, noise=.5, random_state=920):
-    """
-     Returns the two spirals dataset.
-    """
-    n = np.sqrt(np.random.rand(n_points,1)) * 600 * (2*np.pi)/360
-    d1x = -1.5*np.cos(n)*n + np.random.randn(n_points,1) * noise
-    d1y =  1.5*np.sin(n)*n + np.random.randn(n_points,1) * noise
-    return (np.vstack((np.hstack((d1x,d1y)),np.hstack((-d1x,-d1y)))),
-            np.hstack((np.zeros(n_points),np.ones(n_points))))
+from hess import data
+import hess.nets as models
+from parser import parser
+
+
 
 def main():
-    X, Y = twospirals(500, noise=1.3)
-    train_x = torch.FloatTensor(X)
-    train_y = torch.FloatTensor(Y).unsqueeze(-1)
+    args = parser()
+    args.device = None
+    if torch.cuda.is_available():
+        args.device = torch.device("cuda")
+    else:
+        args.device = torch.device("cpu")
 
-    use_cuda = torch.cuda.is_available()
-    if use_cuda:
-        torch.cuda.set_device(2)
-        torch.set_default_tensor_type(torch.cuda.FloatTensor)
-        train_x, train_y = train_x.cuda(), train_y.cuda()
+    print("Preparing directory %s" % args.dir)
+    os.makedirs(args.dir, exist_ok=True)
+    with open(os.path.join(args.dir, "command.sh"), "w") as f:
+        f.write(" ".join(sys.argv))
+        f.write("\n")
+
+    torch.backends.cudnn.benchmark = True
+    torch.manual_seed(args.seed)
+    torch.cuda.manual_seed(args.seed)
+
+    print("Using model %s" % args.model)
+    model_cfg = getattr(models, args.model)
+
+    print("Loading dataset %s from %s" % (args.dataset, args.data_path))
+    loaders, num_classes = data.loaders(
+        args.dataset,
+        args.data_path,
+        args.batch_size,
+        args.num_workers,
+        model_cfg.transform_train,
+        model_cfg.transform_test,
+        use_validation=not args.use_test,
+        split_classes=args.split_classes,
+    )
+
+
 
     loss_func = torch.nn.BCEWithLogitsLoss()
     lr = 0.01
@@ -36,13 +58,18 @@ def main():
     init_eigs = []
     final_eigs = []
     pct_keep = 0.4
-    optim = torch.optim.Adam
+    optim = torch.optim.SGD
 
     for trial in range(n_trials):
-        model = hess.nets.cifar_net(train_x, train_y, bias=True, 
-                                n_hidden=5, hidden_size=10,
-                                activation=torch.nn.ELU(),
-                                pct_keep=pct_keep)
+        print("Preparing model")
+        print(*model_cfg.args)
+        model = model_cfg.base(*model_cfg.args, num_classes=num_classes, **model_cfg.kwargs)
+        model.to(args.device)
+
+        # model = hess.nets.cifar_net(train_x, train_y, bias=True, 
+        #                         n_hidden=5, hidden_size=10,
+        #                         activation=torch.nn.ELU(),
+        #                         pct_keep=pct_keep)
 
         if use_cuda:
             model = model.cuda()
