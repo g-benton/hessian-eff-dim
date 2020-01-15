1f2046e3def15c438bce197ad526976b5386a237
Wesley Maddox <wjm363@nyu.edu>
2020-01-15 17:11:06 -0500

    refactor experiments

---
+++ experiments/bnn_spirals/bnn.py
@@ -0,0 +1,154 @@
+# Copyright Contributors to the Pyro project.
+# SPDX-License-Identifier: Apache-2.0
+
+"""
+Bayesian Neural Network
+=======================
+
+We demonstrate how to use NUTS to do inference on a simple (small)
+Bayesian neural network with two hidden layers.
+"""
+
+import argparse
+import os
+import time
+
+import matplotlib
+import matplotlib.pyplot as plt
+import numpy as onp
+
+from jax import vmap
+import jax.numpy as np
+import jax.random as random
+from jax.nn import elu, relu
+
+import numpyro
+from numpyro import handlers
+import numpyro.distributions as dist
+from numpyro.infer import MCMC, NUTS
+
+matplotlib.use('Agg')  # noqa: E402
+
+
+# the non-linearity we use in our neural network
+def nonlin(x):
+    #return np.tanh(x)
+    return elu(x)
+
+
+# a two-layer bayesian neural network with computational flow
+# given by D_X => D_H => D_H => D_Y where D_H is the number of
+# hidden units. (note we indicate tensor dimensions in the comments)
+def model(X, Y, D_H):
+
+    D_X, D_Y = X.shape[1], 1
+
+    # sample first layer (we put unit normal priors on all weights)
+    w1 = numpyro.sample("w1", dist.Normal(np.zeros((D_X, D_H)), np.ones((D_X, D_H))))  # D_X D_H
+    z1 = nonlin(np.matmul(X, w1))   # N D_H  <= first layer of activations
+
+    # sample second layer
+    w2 = numpyro.sample("w2", dist.Normal(np.zeros((D_H, D_H)), np.ones((D_H, D_H))))  # D_H D_H
+    z2 = nonlin(np.matmul(z1, w2))  # N D_H  <= second layer of activations
+
+    # sample final layer of weights and neural network output
+    w3 = numpyro.sample("w3", dist.Normal(np.zeros((D_H, D_Y)), np.ones((D_H, D_Y))))  # D_H D_Y
+    z3 = np.matmul(z2, w3)  # N D_Y  <= output of the neural network
+
+    # we put a prior on the observation noise
+    prec_obs = numpyro.sample("prec_obs", dist.Gamma(3.0, 1.0))
+    sigma_obs = 1.0 / np.sqrt(prec_obs)
+
+    # observe data
+    numpyro.sample("Y", dist.Normal(z3, sigma_obs), obs=Y)
+
+
+# helper function for HMC inference
+def run_inference(model, args, rng_key, X, Y, D_H):
+    start = time.time()
+    kernel = NUTS(model)
+    mcmc = MCMC(kernel, args.num_warmup, args.num_samples, num_chains=args.num_chains,
+                progress_bar=False if "NUMPYRO_SPHINXBUILD" in os.environ else True)
+    mcmc.run(rng_key, X, Y, D_H)
+    mcmc.print_summary()
+    print('\nMCMC elapsed time:', time.time() - start)
+    return mcmc.get_samples()
+
+
+# helper function for prediction
+def predict(model, rng_key, samples, X, D_H):
+    model = handlers.substitute(handlers.seed(model, rng_key), samples)
+    # note that Y will be sampled in the model because we pass Y=None here
+    model_trace = handlers.trace(model).get_trace(X=X, Y=None, D_H=D_H)
+    return model_trace['Y']['value']
+
+
+# create artificial regression dataset
+def get_data(N=50, D_X=3, sigma_obs=0.05, N_test=500):
+    D_Y = 1  # create 1d outputs
+    onp.random.seed(0)
+    X = np.linspace(-1, 1, N)
+    X = np.power(X[:, onp.newaxis], np.arange(D_X))
+    W = 0.5 * onp.random.randn(D_X)
+    Y = np.dot(X, W) + 0.5 * np.power(0.5 + X[:, 1], 2.0) * np.sin(4.0 * X[:, 1])
+    Y += sigma_obs * onp.random.randn(N)
+    Y = Y[:, onp.newaxis]
+    Y -= np.mean(Y)
+    Y /= np.std(Y)
+
+    assert X.shape == (N, D_X)
+    assert Y.shape == (N, D_Y)
+
+    X_test = np.linspace(-1.3, 1.3, N_test)
+    X_test = np.power(X_test[:, onp.newaxis], np.arange(D_X))
+
+    return X, Y, X_test
+
+
+def main(args):
+    N, D_X, D_H = args.num_data, 3, args.num_hidden
+    X, Y, X_test = get_data(N=N, D_X=D_X)
+
+    # do inference
+    rng_key, rng_key_predict = random.split(random.PRNGKey(0))
+    samples = run_inference(model, args, rng_key, X, Y, D_H)
+
+    # predict Y_test at inputs X_test
+    vmap_args = (samples, random.split(rng_key_predict, args.num_samples * args.num_chains))
+    predictions = vmap(lambda samples, rng_key: predict(model, rng_key, samples, X_test, D_H))(*vmap_args)
+    predictions = predictions[..., 0]
+
+    # compute mean prediction and confidence interval around median
+    mean_prediction = np.mean(predictions, axis=0)
+    percentiles = onp.percentile(predictions, [5.0, 95.0], axis=0)
+
+    # make plots
+    fig, ax = plt.subplots(1, 1)
+
+    # plot training data
+    ax.plot(X[:, 1], Y[:, 0], 'kx')
+    # plot 90% confidence level of predictions
+    ax.fill_between(X_test[:, 1], percentiles[0, :], percentiles[1, :], color='lightblue')
+    # plot mean prediction
+    ax.plot(X_test[:, 1], mean_prediction, 'blue', ls='solid', lw=2.0)
+    ax.set(xlabel="X", ylabel="Y", title="Mean predictions with 90% CI")
+
+    plt.savefig('bnn_plot.pdf')
+    plt.tight_layout()
+
+
+if __name__ == "__main__":
+    assert numpyro.__version__.startswith('0.2.3')
+    parser = argparse.ArgumentParser(description="Bayesian neural network example")
+    parser.add_argument("-n", "--num-samples", nargs="?", default=2000, type=int)
+    parser.add_argument("--num-warmup", nargs='?', default=1000, type=int)
+    parser.add_argument("--num-chains", nargs='?', default=1, type=int)
+    parser.add_argument("--num-data", nargs='?', default=100, type=int)
+    parser.add_argument("--num-hidden", nargs='?', default=5, type=int)
+    parser.add_argument("--device", default='cpu', type=str, help='use "cpu" or "gpu".')
+    args = parser.parse_args()
+
+    numpyro.set_platform(args.device)
+    numpyro.set_host_device_count(args.num_chains)
+
+    main(args)
